2417e2926afc99eb75c6ecfb27bf2bbd8e4a797f
Reazell <kapitankopytko@gmail.com>
2017-11-12 19:26:21 +0100

    Basic controls
    Added: cutting grass, planting seed.

---
+++ project-files/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public static string currentTool = "none";
+    public static int seedCount = 0;
+    public static int hopsCount = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ project-files/Scripts/PlantController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlantController : MonoBehaviour
+{
+
+    public Sprite T_Grass;
+    public Sprite T_Dirt;
+    public Sprite T_Seed;
+    public Sprite Stage1;
+    public Sprite Stage2;
+    public Sprite ReadyHarvest;
+    public SpriteRenderer[] spriteRenderer;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+
+        if (GameController.currentTool == "hoe")
+        {
+            GetComponent<SpriteRenderer>().sprite = T_Dirt;
+        }
+
+        if (GameController.currentTool == "seed")
+        {
+            if (GetComponent<SpriteRenderer>().sprite != T_Dirt)
+            {
+                Debug.Log("Not dirt");
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = T_Seed;
+                Debug.Log("Seed planted");
+
+            }
+        }
+
+        //if (GetComponent<SpriteRenderer>().sprite = ReadyHarvest)
+        //{
+        //    GameController.hopsCount++;
+        //    Debug.Log(GameController.hopsCount);
+        //    GetComponent<SpriteRenderer>().sprite = T_Dirt;
+        //}
+    }
+}
